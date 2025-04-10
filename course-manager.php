417b9d8f8572778580bf9ef21d720fe675f2c1b1
marius <marius@coretrek.no>
2025-04-10 20:26:19 +0200

    chore: update plugin version to 0.1.0-beta.1

--- course-manager.php
+++ course-manager.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Course Manager
  * Description: A WordPress plugin for managing course listings and enrollments, tailored for Norwegian users.
- * Version: 1.0
+ * Version: 0.1.0-beta.1
  * Requires Plugins: woocommerce
  */
 
